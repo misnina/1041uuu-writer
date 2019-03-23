88b145b24de0c00161d9816d4d8bfbb7b867c2c8
Nina <ninarachaebuie@gmail.com>
2019-03-23 13:57:47 -0500

    Updates

---
+++ precache-manifest.3958c6d18e3de04508b0229ffb621bbd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "de92afaa3e893499e4f3af7ab4eea39d",
+    "url": "/1041uuu-writer/static/media/g.de92afaa.gif"
+  },
+  {
+    "revision": "643ee7191c4fef62668d",
+    "url": "/1041uuu-writer/static/css/main.964dd63d.chunk.css"
+  },
+  {
+    "revision": "36e2ef6f705130051c19",
+    "url": "/1041uuu-writer/static/js/runtime~main.4d4bb79a.js"
+  },
+  {
+    "revision": "bda78f35974c88c1e3de",
+    "url": "/1041uuu-writer/static/js/2.e829eba1.chunk.js"
+  },
+  {
+    "revision": "a753a1d7a63ceebebd970643b59f2cde",
+    "url": "/1041uuu-writer/static/media/f.a753a1d7.gif"
+  },
+  {
+    "revision": "9298b8aa90e9bc71a6162878ee24cbeb",
+    "url": "/1041uuu-writer/static/media/e.9298b8aa.gif"
+  },
+  {
+    "revision": "c40db678aa354ba91257f6258a1521e1",
+    "url": "/1041uuu-writer/static/media/c.c40db678.gif"
+  },
+  {
+    "revision": "0a8b1950a48a4ee96ba53ac7963beb5f",
+    "url": "/1041uuu-writer/static/media/PokemonDPPt.0a8b1950.ttf"
+  },
+  {
+    "revision": "643ee7191c4fef62668d",
+    "url": "/1041uuu-writer/static/js/main.4dffd932.chunk.js"
+  },
+  {
+    "revision": "c84ee4c9e004b2fa3e4d6c194164c33a",
+    "url": "/1041uuu-writer/static/media/j.c84ee4c9.gif"
+  },
+  {
+    "revision": "558607a786cf17fd0fa766df94bb0226",
+    "url": "/1041uuu-writer/static/media/b.558607a7.gif"
+  },
+  {
+    "revision": "3549beaae0ba185e62d53e57144caa0d",
+    "url": "/1041uuu-writer/static/media/a.3549beaa.gif"
+  },
+  {
+    "revision": "3559ce9d0ac06403a6695f4c659884b5",
+    "url": "/1041uuu-writer/static/media/h.3559ce9d.gif"
+  },
+  {
+    "revision": "758941f0e885597f8417ae7737a4b83d",
+    "url": "/1041uuu-writer/static/media/d.758941f0.gif"
+  },
+  {
+    "revision": "0a766f6015ea33b18f312856ac59c6e0",
+    "url": "/1041uuu-writer/static/media/i.0a766f60.gif"
+  },
+  {
+    "revision": "cba7d86a5b7d3c5c157bd8998c98cdc6",
+    "url": "/1041uuu-writer/index.html"
+  }
+];
